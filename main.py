6893cf5d79eb28044c82fde93dece244e9e7f2ec
fabiansj0028 <49877808+fabiansj0028@users.noreply.github.com>
2021-03-19 03:54:59 -0600

    [ADD]:Función de Restricciones

--- main.py
+++ main.py
@@ -14,3 +14,43 @@ def setAnswer():
 		size = len(cards[i])
 		answer +=[cards[i][randomIndex(size)]]
 	return answer
+
+        
+def setRestrictions(n):
+        restricciones = []
+        answer = setAnswer()
+        categorias = ['Sospechosos','Arma','Motivo','Lugar']
+        
+        #Contador que indica el numero de parejas de restricciones
+        for contador1 in range(n):
+                restriccion = []
+                
+                categoria1 = 100 #Categorias simplemente para validar
+                categoria2 = 0
+                i=0
+
+                #Ciclo para obtener el numero de restricciones
+                while(i<2):
+                        numRandom = random.randint(0,3)
+                        size = len(cards[categorias[numRandom]])
+                        cartaActual = cards[categorias[numRandom]][randomIndex(size)]
+                        
+                        if(categoria1 != 100):
+                                if(numRandom == categoria1):
+                                        continue
+                                else:
+                                        restriccion += [cartaActual]
+                                        i+=1
+                                        categoria2 = numRandom
+                        else:       
+                                categoria1 = numRandom
+                                restriccion += [cartaActual]
+                                i+=1
+                prueba = [answer[categoria1],answer[categoria2]]
+                if(restriccion != prueba):
+                        restricciones += [restriccion]
+                else:
+                        n-=1                                     
+                
+                
+        
