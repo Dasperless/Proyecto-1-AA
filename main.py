d06e654f88307ba9096e2227b6c7ff373ccd7517
fabiansj0028 <49877808+fabiansj0028@users.noreply.github.com>
2021-03-30 22:32:06 -0600

    [CHG]:Funcion de Restricciones

--- main.py
+++ main.py
@@ -46,48 +46,41 @@ def bruteForceSolution(cardsList, solution):
 					res = [sospechoso,arma,motivo,lugar]
 					print(res)
 					if(res == solution):
-						return res
-					
-	
+						return res					
 
-	
-def setRestrictions(n):
-	restricciones = []
-	answer = setAnswer()
-	categorias = ['Sospechosos','Arma','Motivo','Lugar']
-	
-	#Contador que indica el numero de parejas de restricciones
-	for contador1 in range(n):
-		restriccion = []
-		
-		categoria1 = 100 #Categorias simplemente para validar
-		categoria2 = 0
-		i=0
+	"""Retorna una lista de restricciones.
 
-		#Ciclo para obtener el numero de restricciones
-		while(i<2):
-			numRandom = random.randint(0,3)
-			size = len(cards[categorias[numRandom]])
-			cartaActual = cards[categorias[numRandom]][randomIndex(size)]
-			
-			if(categoria1 != 100):
-				if(numRandom == categoria1):
-					continue
-				else:
-					restriccion += [cartaActual]
-					i+=1
-					categoria2 = numRandom
-			else:       
-				categoria1 = numRandom
-				restriccion += [cartaActual]
-				i+=1
-		prueba = [answer[categoria1],answer[categoria2]]
-		if(restriccion != prueba):
-			restricciones += [restriccion]
-		else:
-			n-=1                                     
-		
-		
-sol = setAnswer(cards)	
-print(sol)
-print(bruteForceSolution(cards, sol))
+        Args:
+                n: Cantidad de restricciones
+                sol: Solucion del juego
+
+        Returns:
+                list: Lista de restricciones
+        """
+def setRestrictions(n,sol):
+        restricciones = []
+        categorias = ['Sospechosos','Arma','Motivo','Lugar']
+        contador1=0
+        while(contador1<n):
+                restriccion=[]
+                contador2=0
+                categoria1=0
+                categoria2=0
+                while(contador2<2):
+                        numRandom = random.randint(0,3)
+                        size = len(cards[categorias[numRandom]])
+                        cartaActual = cards[categorias[numRandom]][randomIndex(size)]
+                        if(contador2==0):
+                                categoria1=numRandom
+                                restriccion+=[cartaActual]
+                                contador2+=1
+                        else:
+                                categoria2=numRandom
+                                if(cartaActual!=restriccion[0]):    
+                                        restriccion+=[cartaActual]
+                                        contador2+=1
+                prueba = [sol[categoria1],sol[categoria2]]
+                if(restriccion!=prueba):
+                        restricciones+=[restriccion]
+                        contador1+=1
+        return restricciones
